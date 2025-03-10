4e831b02b878057146583f76d893933af4b0288a
Lekha827 <bollinenilekha@gmail.com>
2025-03-09 21:57:40 -0400

    Fix the folder name

--- Assets/js/skills.js
+++ Assets/js/skills.js
@@ -1,32 +1,32 @@
 const skillsData = {
     frontend: [
-        { name: 'JavaScript', logo: 'assets/img/skills/javascript.png' },
-        { name: 'Vue.js', logo: 'assets/img/skills/vue.png' },
-        { name: 'React.js', logo: 'assets/img/skills/react.png' },
-        { name: 'HTML', logo: 'assets/img/skills/html.png' },
-        { name: 'CSS', logo: 'assets/img/skills/css.png' },
-        { name: 'Bootstrap', logo: 'assets/img/skills/bootstrap.png' }
+        { name: 'JavaScript', logo: 'Assets/img/skills/javascript.png' },
+        { name: 'Vue.js', logo: 'Assets/img/skills/vue.png' },
+        { name: 'React.js', logo: 'Assets/img/skills/react.png' },
+        { name: 'HTML', logo: 'Assets/img/skills/html.png' },
+        { name: 'CSS', logo: 'Assets/img/skills/css.png' },
+        { name: 'Bootstrap', logo: 'Assets/img/skills/bootstrap.png' }
     ],
     backend: [
-        { name: 'Python', logo: 'assets/img/skills/python.png' },
-        { name: 'Django', logo: 'assets/img/skills/django.png' },
-        { name: 'Flask', logo: 'assets/img/skills/flask.png' },
-        { name: 'Redis', logo: 'assets/img/skills/redis.png' },
-        { name: 'Celery', logo: 'assets/img/skills/celery.png' }
+        { name: 'Python', logo: 'Assets/img/skills/python.png' },
+        { name: 'Django', logo: 'Assets/img/skills/django.png' },
+        { name: 'Flask', logo: 'Assets/img/skills/flask.png' },
+        { name: 'Redis', logo: 'Assets/img/skills/redis.png' },
+        { name: 'Celery', logo: 'Assets/img/skills/celery.png' }
     ],
     database: [
-        { name: 'MySQL', logo: 'assets/img/skills/mysql.png' },
-        { name: 'Sybase', logo: 'assets/img/skills/sybase.png' },
-        { name: 'MariaDB', logo: 'assets/img/skills/mariadb.png' },
-        { name: 'MongoDB', logo: 'assets/img/skills/mongodb.png' }
+        { name: 'MySQL', logo: 'Assets/img/skills/mysql.png' },
+        { name: 'Sybase', logo: 'Assets/img/skills/sybase.png' },
+        { name: 'MariaDB', logo: 'Assets/img/skills/mariadb.png' },
+        { name: 'MongoDB', logo: 'Assets/img/skills/mongodb.png' }
     ],
     ops: [
-        { name: 'Docker', logo: 'assets/img/skills/docker.png' },
-        { name: 'AWS', logo: 'assets/img/skills/aws.png' },
-        { name: 'NGINX', logo: 'assets/img/skills/nginx.png' },
-        { name: 'Jenkins', logo: 'assets/img/skills/jenkins.png' },
-        { name: 'Octopus', logo: 'assets/img/skills/octopus.png' },
-        { name: 'CUDA', logo: 'assets/img/skills/cuda.png' }
+        { name: 'Docker', logo: 'Assets/img/skills/docker.png' },
+        { name: 'AWS', logo: 'Assets/img/skills/aws.png' },
+        { name: 'NGINX', logo: 'Assets/img/skills/nginx.png' },
+        { name: 'Jenkins', logo: 'Assets/img/skills/jenkins.png' },
+        { name: 'Octopus', logo: 'Assets/img/skills/octopus.png' },
+        { name: 'CUDA', logo: 'Assets/img/skills/cuda.png' }
     ]
 };
 
